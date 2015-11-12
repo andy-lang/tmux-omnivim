afa3ce968f9090e638bd432da0e2a2e78b421003
andy-lang <langy26@gmail.com>
2015-11-12 22:58:07 +1030

    Add reading filenames from command line.
    Add documentation comments also.

--- omnivim.py
+++ omnivim.py
@@ -1,42 +1,87 @@
 import os, sys
 import subprocess
+import re
 
-# check that the editor is set to vim or something similar
-# TODO: get this working with Neovim, Gvim, etc.
-# EDITOR = os.environ.get('EDITOR', 'vim') if os.environ.get('EDITOR') else 'vim'
-EDITOR = "gvim"
-EDITOR_FLAGS = "-v"
-DEBUG = False
+def call_neovim(editor, editor_flags, files):
+	"""Call neovim with a desired number of flags and files.
 
-# check that tmux is currently running. This will be None if no tmux environment variable has been set
-IN_TMUX = os.environ.get('TMUX')
+	This is done in a separate function as neovim has a VERY different way of doing things.
 
-# TODO: read this from command line arguments
-files = ["omnivim.py", "omnivim.sh"]
+	If a running Vim server is found in the current Tmux window, then the files are opened there. Otherwise, a new Vim server instance is created in the pane where the call to this program was made.
 
+	Args:
+		editor (str): The editor command that should be called.
+		editor_flags (str): A list of strings containing extra flags that should be called alongside the editor of choice.
+		files (str): A list of strings containing the files that should be opened.
+	"""
+	print "TODO: call neovim"
 
-if IN_TMUX is not None:
+def call_vim(editor, editor_flags, files):
+	"""Call vim (or a variant thereof) with a desired number of flags and files.
+
+	If a running Vim server is found in the current Tmux window, then the files are opened there. Otherwise, a new Vim server instance is created in the pane where the call to this program was made.
+
+	Args:
+		editor (str): The editor command that should be called.
+		editor_flags (str): A list of strings containing extra flags that should be called alongside the editor of choice.
+		files (str): A list of strings containing the files that should be opened.
+	"""
+
 	# get the current tmux window in which this command was run
 	tmux_current_window = subprocess.check_output(["tmux", "display-message", "-p", "#{window_id}"]).rstrip()
-	if DEBUG:
-		print "current window: |" + tmux_current_window + "|"
 
-	vim_server_list = str.split(subprocess.check_output([EDITOR, '--serverlist']).rstrip())
-	if DEBUG:
-		print "vim servers:", vim_server_list
+	vim_server_list = str.split(subprocess.check_output([editor] + editor_flags + ['--serverlist']).rstrip())
 
 	for name in vim_server_list:
-		if DEBUG:
-			print "name: " + name
 		if name == tmux_current_window:
-			subprocess.call([EDITOR, EDITOR_FLAGS, "--servername", name, "--remote"] + files)
+			subprocess.call([editor] + editor_flags + ["--servername", name, "--remote"] + files)
 			sys.exit(0)
 
+	print files
 	# if we hit this point, we didn't find a server.
 	# So just create a new Vim server with the required arguments
-	subprocess.call([EDITOR, EDITOR_FLAGS, "--servername", tmux_current_window, "--remote-silent"] + files)
+	subprocess.call([editor] + editor_flags + ["--servername", tmux_current_window, "--remote-silent"] + files)
 
-else:
-	# otherwise we're not in tmux.
-	# So just launch vim, business as usual, with no added server shenanigans
-	subprocess.call([EDITOR, EDITOR_FLAGS] + files)
+def main():
+	# work out the editor.
+
+	# if the OMNIVIM_EDITOR environment variable is set, use it.
+	# otherwise, if the EDITOR variable is set, use that instead.
+	# otherwise, use vim.
+	EDITOR = 'vim'
+	if os.environ.get('OMNIVIM_EDITOR') is not None:
+		EDITOR = os.environ.get('OMNIVIM_EDITOR')
+	elif os.environ.get('EDITOR') is not None:
+		EDITOR = os.environ.get('EDITOR')
+	
+	# similar for editor flags. If OMNIVIM_EDITOR_FLAGS is set, use that.
+	# otherwise, use nothing.
+	EDITOR_FLAGS = []
+	if os.environ.get('OMNIVIM_EDITOR_FLAGS') is not None:
+		EDITOR_FLAGS = str.split(os.environ.get('OMNIVIM_EDITOR_FLAGS').rstrip(), ' ')
+		
+
+	# read files from command line arguments
+	# we remove the first, since this will always be omnivim.py
+	files = sys.argv
+	files.pop(0)
+	print "files:", files
+	
+	# checking the TMUX environment variable will confirm whether or not tmux is currently running.
+	in_tmux = os.environ.get('TMUX')
+
+	if in_tmux is not None:
+		# if tmux is running, then we have different things to do if using neovim or g/vim
+		# use regex to work it out
+		is_neovim = re.search('n(eo)?vim', EDITOR)
+		is_vim = re.search('g?vim', EDITOR)
+
+		if is_neovim is not None:
+			call_neovim(EDITOR, EDITOR_FLAGS, files)
+		elif is_vim is not None:
+			call_vim(EDITOR, EDITOR_FLAGS, files)
+	else:
+		# otherwise, just call the editor and flags with the requisite files, no strings (or servers) attached.
+		subprocess.call([editor] + editor_flags + files)
+
+main()
